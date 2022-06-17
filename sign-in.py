9126ee771447df30652b799d06ac79224d87ae75
fasetbook <107675067+fasetbook@users.noreply.github.com>
2022-06-17 08:47:22 +0000

    Add files via upload

---
+++ sign-in.py
@@ -0,0 +1,22 @@
+a=3
+name=["jack","mili","james","meiya","Hacker"]
+pwd=["jack888","mili777","james666","meiya555"]
+jack1='888'
+mili='777'
+james='666'
+meiya='555'
+
+for i in range(3):
+    namesr=input('Please enter your username:')
+    if namesr in name:  #The user name exists and continues to enter the password.
+        pwdsr=input('Please enter password:')
+        if namesr+pwdsr in pwd:
+            print('Welcome to the popular server')
+            break
+        else:
+            a-=1
+            print('Account or password error,You have '+str(a)+' more opportunities\n')
+
+    else:
+        a-=1
+        print('The account doesn't exist! You have'+str(a)+'more opportunities\n')
